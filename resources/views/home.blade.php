94c338749efb1e8afadc9c90522d2739a8aec58b
Saper330 <161034123+Saper330@users.noreply.github.com>
2024-03-25 00:24:59 +0300

    Added a slider

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,63 +7,55 @@
 <div class="home">
 
   <!-- Главная секция -->
-  <div class="main-section">
-    <div class="main-slider">
-      <!-- Слайд -->
-      <div class="slide">
-        <!-- Фоновая картинка -->
-        <img src="/img/main-slider-background1.jpg" alt="">
-        <!-- Картинка с машиной -->
-        <img src="/img/main-slider-car1.png" alt="">
-        <!-- Прямоугольник с названием -->
-        <div class="title">
-          <div class="text">Автоцистерны</div>
-        </div>
-      </div>
-      <!-- Пагинация -->
-      <div class="slider-pagination">
-        <!-- Точка обычная -->
-        <div class="dot"></div>
-        <!-- Точка активная -->
-        <div class="dot active"></div>
+  <div class="main-slider">
+    <!-- Слайд -->
+    <div class="slide ">
+      <!-- Фоновая картинка -->
+        <img class="img_fon" src="img/image_12.png" alt="фон">
+        <img class="img_car" src="img/main-slider-car1.png" alt="урал 4320">
+        <img class="svg_vector" src="img/Vector_1.svg" alt="тень">
+      <div class="title">
+        <p>Aвтоцистерны</p>
       </div>
     </div>
-  </div>
-
-
-  <div class="container">
-    <p class='kt'>каталог техники</p>
-    <div class="eq_catalog">
-      <div class="eq_item">
-        <img src="img/image_15.png" alt="камаз-55510">
-        <p>автоцистерны</p>
-      </div>
-      <span class='vline'></span>
-      <div class="eq_item">
-        <img src="img/pngegg-1536x817_1.png" alt="камаз-55510">
-        <p>прицепная<br>техника</p>
-      </div>
-      <span class='vline'></span>
-      <div class="eq_item">
-        <img src="img/30975084_1.png" alt="камаз-55510">
-        <p>автокраны и<br>манипуляторы</p>
-      </div>
-      <span class='vline'></span>
-      <div class="eq_item">
-        <img src="img/image_20.png" alt="камаз-55510">
-        <p>технологический<br>транспорт</p>
-      </div>
+    <!-- Пагинация -->
+    <div class="slider-pagination">
+      <div class="dot_active"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
+      <div class="dot"></div>
     </div>
-    <button href='#' class='btn_catalog'>
-      перейти в каталог
-    </button> 
-  </div>
 </div>
+  
 
 
-<!-- Тестирование фоновой картинки через свойство background -->
-<div class="sldr"></div>
-
-
+<div class="container">
+  <p class='kt'>каталог техники</p>
+  <div class="eq_catalog">
+    <div class="eq_item">
+      <img src="img/image_15.png" alt="камаз-55510">
+      <p>автоцистерны</p>
+    </div>
+    <span class='vline'></span>
+    <div class="eq_item">
+      <img src="img/pngegg-1536x817_1.png" alt="камаз-55510">
+      <p>прицепная<br>техника</p>
+    </div>
+    <span class='vline'></span>
+    <div class="eq_item">
+      <img src="img/30975084_1.png" alt="камаз-55510">
+      <p>автокраны и<br>манипуляторы</p>
+    </div>
+    <span class='vline'></span>
+    <div class="eq_item">
+      <img src="img/image_20.png" alt="камаз-55510">
+      <p>технологический<br>транспорт</p>
+    </div>
+  </div>
+  <button href='#' class='btn_catalog'>
+    перейти в каталог
+  </button> 
+</div>
 
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,7 +9,10 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   @yield('robots')
   <link rel="shortcut icon" href="{{ asset('/img/favicon.svg') }}" type="image/x-icon">
-  <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">-->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
   <title>Спецтехника</title>
   @yield('style')
   @vite(['resources/sass/main.scss'])
@@ -43,9 +46,6 @@
           <a href="#">о компании</a>
         </div>  
       </div>
-    </div>
-    <!-- Слайдер перенеси в главную секцию -->
-    <div class="sldr">
     </div>
   </header>
 
@@ -90,6 +90,6 @@
 
   @yield('script')
   @vite(['resources/js/main.js'])
-  <!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>-->
+  <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> -->
 </body>
 </html>
