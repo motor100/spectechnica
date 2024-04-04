0dcd40747c8d72602b50881bda928c1c8870dc83
Evgeny <260443@mail.ru>
2024-04-04 22:26:06 +0500

    Add favicon.svg

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,10 +9,6 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   @yield('robots')
   <link rel="shortcut icon" href="{{ asset('/img/favicon.svg') }}" type="image/x-icon">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
   <title>Спецтехника</title>
   @yield('style')
   @vite(['resources/sass/main.scss'])
