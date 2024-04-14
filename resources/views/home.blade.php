0a3a5964d6d31d4d2cfea7deba8811abcb0dd022
Evgeny <260443@mail.ru>
2024-04-14 20:02:44 +0500

    Home page about-section replace images from svg to png

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -86,25 +86,25 @@
         </div>
         <div class="col-md-3">
           <div class="item_s">
-            <img src="/img/s_1.svg" class="svg_item_image" alt="#">
+            <img src="/img/s_1.png" class="svg_item_image" alt="#">
             <p>Высокое качество<br>продукции по<br>доступным ценам</p>  
           </div>
         </div>
         <div class="col-md-3">
           <div class="item_s">
-            <img src="/img/s_2.svg" class="svg_item_image" alt="#">
+            <img src="/img/s_2.png" class="svg_item_image" alt="#">
             <p>Штат опытных<br>сотрудников</p>
           </div>
         </div>
         <div class="col-md-3">
           <div class="item_s">
-            <img src="/img/s_3.svg" class="svg_item_image" alt="#">
+            <img src="/img/s_3.png" class="svg_item_image" alt="#">
             <p>Trade-in: принимаем<br>Вашу технику в зачет</p>
           </div>
         </div>
         <div class="col-md-3">
           <div class="item_s">
-            <img src="/img/s_4.svg" class="svg_item_image" alt="#">
+            <img src="/img/s_4.png" class="svg_item_image" alt="#">
             <p>Льготная доставка в<br>регионы собственным<br>автотранспортом</p>
           </div>
         </div>
