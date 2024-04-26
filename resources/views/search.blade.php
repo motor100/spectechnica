f92487ba9376ae38b465e3c68228e060436a2d24
Evgeny <260443@mail.ru>
2024-04-26 22:47:35 +0500

    Page search

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -6,17 +6,47 @@
 
 <div class="search">
 
-    <div class="serach-rezult">
-        <div class="container">
-            Результаты поиска
-        </div>
+  <!-- Хлебные крошки -->
+  <div class="breadcrumbs">
+    <div class="container">
+      <a href="/">Главная</a>
+      <span>-</span>
+      <span class="active">Поиск</span>
     </div>
+  </div>
 
-    <!-- Форма заполнения заявки -->
-    @include('application-form-section')
-  
-    <!-- Секция с производственными характеристиками -->
-    @include('advantages-section')
+  <!-- Форма поиска -->
+  @include('search-form-section')
+
+  <div class="search-rezult">
+    <div class="container">
+
+      <!-- Если товары найдены -->
+      <div class="page-title">Результаты поиска "Вакуумный агрегат АЦПТ 1612"</div>
+      <div class="rezult">
+        <div class="rezult-item">
+          <a href="#">Вакуумный агрегат АЦПТ 1612</a>
+        </div>
+        <div class="rezult-item">
+          <a href="#">Вакуумный агрегат АЦПТ 1612</a>
+        </div>
+        <div class="rezult-item">
+          <a href="#">Вакуумный агрегат АЦПТ 1612</a>
+        </div>
+      </div>
+
+      <!-- Если товаров не найдено -->
+      <div class="page-title">ПО ЗАПРОСУ: "Вакуумный агрегат АЦПТ 1612" <br>Товаров не найдено</div>
+      <a href="/catalog" class="to-catalog-btn">В каталог</a>
+
+    </div>
+  </div>
+
+  <!-- Форма заполнения заявки -->
+  @include('application-form-section')
+
+  <!-- Секция с производственными характеристиками -->
+  @include('advantages-section')
 
 </div>
 
